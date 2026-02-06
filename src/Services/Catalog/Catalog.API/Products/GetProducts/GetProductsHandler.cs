cd1a02e13a2f51d6ac97855419b41d4356dc27b1
Anup Mondal <anupmail110@gmail.com>
2026-02-07 01:40:56 +0530

    GetProducts, GetProductById, GetProductByCategory implemented

---
+++ src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsHandler.cs
@@ -0,0 +1,21 @@
+﻿using BuildingBlocks.CQRS;
+using Catalog.API.Models;
+using Marten;
+
+namespace Catalog.API.Products.GetProducts
+{
+    public record GetProductsQuery():IQuery<GetProductsResult>;
+    public record GetProductsResult(IEnumerable<Product> Products);
+    public class GetProductsQueryHandler(IDocumentSession session, ILogger<GetProductsQueryHandler> logger) 
+        : IQueryHandler<GetProductsQuery, GetProductsResult>
+    {
+        public async Task<GetProductsResult> Handle(GetProductsQuery query, CancellationToken cancellationToken)
+        {
+            logger.LogInformation($"GetProductsQueryHandler.Handle method called with {query}");
+            
+            var products = await session.Query<Product>().ToListAsync(cancellationToken);
+
+            return new GetProductsResult(products);
+        }
+    }
+}
