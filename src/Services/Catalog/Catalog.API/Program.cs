6f957eecfe4516f48a4091008003a00d8c8718e9
Anup Mondal <anupmail110@gmail.com>
2026-02-09 02:00:38 +0530

    global exception handling

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,5 +1,3 @@
-using BuildingBlocks.Behaviors;
-using Marten;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -16,10 +14,41 @@ builder.Services.AddCarter();
 builder.Services.AddMarten(ops =>
 {
     ops.Connection(builder.Configuration.GetConnectionString("Database")!);
-}).UseLightweightSessions();  
+}).UseLightweightSessions();
+
+builder.Services.AddExceptionHandler<CustomExceptionHandler>();
 
 var app = builder.Build();
 
 app.MapCarter();
+
+app.UseExceptionHandler(options => { });
+
+#region Handle Global exception using Exception handler lamda
+//app.UseExceptionHandler(exceptionHandlerApp =>
+//{
+//    exceptionHandlerApp.Run(async context =>
+//    {
+//        var exception = context.Features.Get<IExceptionHandlerFeature>()?.Error;
+//        if (exception == null) return;
+
+//        var problemDetails = new ProblemDetails
+//        {
+//            Title = exception.Message,
+//            Status = StatusCodes.Status500InternalServerError,
+//            Detail = exception.StackTrace
+//        };
+
+//        var logger = context.RequestServices.GetRequiredService<ILogger<Program>>();
+//        logger.LogError(exception, exception.Message);
+
+//        context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+//        context.Response.ContentType = "application/problem+json";
+
+//        await context.Response.WriteAsJsonAsync(problemDetails);
+
+//    });
+//});
+#endregion
 
 app.Run();
